22a55b62504694422dbdd1235fd4965507f32e91
Marcelo Joia <sitejoia@hotmail.com>
2023-06-28 14:13:26 -0300

    Catalog Content ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './index.css';
 
+import Container from './componentes/Container';
 import Catalog from './routes/Catalog';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Catalog/>
+      <Container/>
     </>
   )
 }
---
+++ src/componentes/Container/index.tsx
@@ -0,0 +1,63 @@
+import './style.css';
+
+import carImg from '../../assets/car.png';
+
+export default function Container() {
+    return (
+        <>
+            <main>
+
+                <h2>Venha nos visitar</h2>
+                <div className="container-ct">
+
+                    <section className="card-content">
+                        <div className="article-card">
+                            <article>
+                                <section>
+                                    <figure>
+                                        <img src={carImg} alt="Carro" />
+                                        <figcaption>
+                                            Lorem ipsum dolor
+                                        </figcaption>
+                                    </figure>
+                                </section>
+                            </article>
+                        </div>
+
+                        <div className='article-card'>
+                            <article>
+                                <section>
+                                    <figure>
+                                        <img src={carImg} alt="Carro" />
+                                        <figcaption>
+                                            Lorem ipsum dolor
+                                        </figcaption>
+                                    </figure>
+                                </section>
+                            </article>
+                        </div>
+                    </section>
+                </div>
+
+                <section className="comment-content">
+                    <h3>O que estão dizendo</h3>
+
+                    <div className="container-ct">
+                        <div className="grid-container-ct">
+
+                            <div className="comment-content-article">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum voluptatem ad fugit id distinctio.</p>
+                            </div>
+
+                            <div className="comment-content-article">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum voluptatem ad fugit id distinctio.</p>
+                            </div>
+
+                        </div>
+                    </div>
+                </section>
+
+            </main >
+        </>
+    );
+}
